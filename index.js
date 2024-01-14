fb49dbd4139ef769bdb0d1e611b1e497126a5829
Victor-MGB <mgbemenaosonduv@gmail.com>
2024-01-14 17:25:36 +0000

    bcryptjs removed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const signupModel = require('./Models/signupModel');
 const path = require('path');
 const bodyParser = require('body-parser');
